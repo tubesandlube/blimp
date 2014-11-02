cf709f80b3c2d3c792624b9bd73bdbffada83522
George Lewis <schvin@schvin.net>
2014-11-02 00:56:47 -0400

    add parameter passing to real `ps`, as-is. this allows for command such as `blimp ls -a`, etc, filters, etc.

--- lib/containers.pm
+++ lib/containers.pm
@@ -2,6 +2,7 @@ package containers;
 
 sub enumerate {
 
+  my $extra;
   my $h;
   my $hosts;
   my $i;
@@ -13,11 +14,17 @@ sub enumerate {
 
   $hosts = hosts::enumerate();
 
+  if($ARGV[1] && $ARGV[1] !~ /^\s*$/) {
+    $extra = join(" ", @ARGV[1..$#ARGV]);
+  } else {
+    $extra = "";
+  }
+
   $out = "";
   for($i = 0; $i <= $#{$hosts}; $i++) {
     #print "attaching to $hosts->[$i]\n";
     docker::drun("hosts active $hosts->[$i]");
-    $out1 = docker::drun("ps");
+    $out1 = docker::drun("ps $extra");
 
     if($out1 && $out1 !~ /^\s*$/) {
       @lines = split(/\n/, $out1);
